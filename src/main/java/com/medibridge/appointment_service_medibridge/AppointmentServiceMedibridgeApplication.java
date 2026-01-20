fd5e088f1525039bd71f51c229e230b3346213ff
omprakashsaggam <omprakash.saggam@gmail.com>
2026-01-20 14:03:05 +0100

    initial commit

---
+++ src/main/java/com/medibridge/appointment_service_medibridge/AppointmentServiceMedibridgeApplication.java
@@ -0,0 +1,13 @@
+package com.medibridge.appointment_service_medibridge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppointmentServiceMedibridgeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppointmentServiceMedibridgeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/medibridge/appointment_service_medibridge/AppointmentServiceMedibridgeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.medibridge.appointment_service_medibridge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppointmentServiceMedibridgeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
